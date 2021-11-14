7dc954bdceacafaf51c2873fc6cc352efb0cb6ae
Travis <73152895+siraht@users.noreply.github.com>
2021-11-13 18:16:22 -0700

    Creation

---
+++ main.js
@@ -0,0 +1,19 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function () {
+        return `${title} by ${author}, ${pages} pages, ${read}`
+    }
+}
+
+function addBookToLibrary() {
+
+}
+
+let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet')
+
+console.log(theHobbit.info());
