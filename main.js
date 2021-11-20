0ae7d9012e7a90570423b3132143e15ae35261a4
siraht <t@travishinton.us>
2021-11-20 15:21:30 -0700

    Refactoring some functions and adding event listener functionality for Read Status buttons

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const body = document.querySelector('.body');
 const cardContainer = document.querySelector('.cardContainer');
-const addButton = document.querySelector('.addButton')
-const closeButton = document.querySelector('.togglePopup')
+const addButton = document.querySelector('.addButton');
+const closeButton = document.querySelector('.togglePopup');
+const titleInput = document.querySelector('.titleInput');
+const authorInput = document.querySelector('.authorInput');
+const pagesInput = document.querySelector('.pagesInput');
+const readInput = document.querySelector('.readInput');
 
-let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'Unread')
-let theHobbit2 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'Unread')
-let theHobbit3 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'Unread')
-let theHobbit4 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'Unread')
-let theHobbit5 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'Unread')
-let theHobbit6 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'Unread')
-let theHobbit7 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'Unread')
+
+let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'true')
+let theHobbit2 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'true')
+let theHobbit3 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'false')
+let theHobbit4 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'true')
+let theHobbit5 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'false')
+let theHobbit6 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'false')
+let theHobbit7 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'true')
 
 addButton.addEventListener('click', event => {
     var x = document.getElementById("popup");
@@ -21,51 +26,98 @@ addButton.addEventListener('click', event => {
 })
 
 closeButton.addEventListener('click', event => {
+    addBookToLibrary();
     var x = document.getElementById("popup");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
+    titleInput.value = ''
+    authorInput.value = ''
+    pagesInput.value = ''
+    readInput.value = ''
 })
 
 
 let myLibrary = [theHobbit, theHobbit2, theHobbit3, theHobbit4, theHobbit5, theHobbit6, theHobbit7];
 
+let addStatusListener = (item) => {
+    item.addEventListener('click', event => {
+        if (item.read === 'true') {
+            item.read = 'false';
+            item.textContent = 'Unread'
+            item.classList.remove('bookRead')
+        } else {
+            item.read = 'true';
+            item.textContent = 'Read'
+            item.classList.add('bookRead')
+        }
+
+    })
+}
+
+function newCardFunction(i) {
+    newCard = document.createElement('div');
+    newCard.id = `${myLibrary[i].title} Card`
+    newCard.classList.add('bookCard');
+}
+
+function bookTitleFunction(i) {
+    bookTitle = document.createElement('h2');
+    bookTitle.textContent = `${myLibrary[i].title}`;
+    bookTitle.classList.add('bookTitle')
+    newCard.appendChild(bookTitle);
+}
+
+function bookAuthorFunction(i) {
+    bookAuthor = document.createElement('h4');
+    bookAuthor.textContent = `${myLibrary[i].author}`;
+    bookAuthor.classList.add('bookAuthor');
+    newCard.appendChild(bookAuthor);
+}
+
+function secondBookDivFunction(i) {
+    secondBookDiv = document.createElement('div');
+    secondBookDiv.id = `Pages and status`;
+    secondBookDiv.classList.add('pagesAndStatus');
+    newCard.appendChild(secondBookDiv);
+}
+
+function bookLengthFunction(i) {
+    bookLength = document.createElement('p');
+    bookLength.textContent = `${myLibrary[i].pages} pp`;
+    bookLength.classList.add('bookLength');
+    secondBookDiv.appendChild(bookLength);
+}
+
+function bookStatusFunction(i) {
+    bookStatus = document.createElement('p');
+    if (myLibrary[i].read === 'true') {
+        bookStatus.textContent = 'Read';
+        bookStatus.classList.add('bookRead');
+    } else {
+        bookStatus.textContent = 'Unread';
+    }
+    bookStatus.classList.add('bookStatus');
+    secondBookDiv.appendChild(bookStatus);
+}
+
 let addCardsToPage = () => {
+    while (cardContainer.firstChild) {
+        cardContainer.removeChild(cardContainer.firstChild);
+    }
     for (let i = 0; i < myLibrary.length; i++) {
-        newCard = document.createElement('div');
-        newCard.id = `${myLibrary[i].title} Card`
-        newCard.classList.add('bookCard');
-
-        bookTitle = document.createElement('h2');
-        bookTitle.textContent = `${myLibrary[i].title}`;
-        bookTitle.classList.add('bookTitle')
-        newCard.appendChild(bookTitle);
-
-        bookAuthor = document.createElement('h4');
-        bookAuthor.textContent = `${myLibrary[i].author}`;
-        bookAuthor.classList.add('bookAuthor');
-        newCard.appendChild(bookAuthor);
-
-        secondBookDiv = document.createElement('div');
-        secondBookDiv.id = `Pages and status`;
-        secondBookDiv.classList.add('pagesAndStatus');
-        newCard.appendChild(secondBookDiv);
-
-        bookLength = document.createElement('p');
-        bookLength.textContent = `${myLibrary[i].pages} pp`;
-        bookLength.classList.add('bookLength');
-        secondBookDiv.appendChild(bookLength);
-
-        bookStatus = document.createElement('p');
-        bookStatus.textContent = `${myLibrary[i].read}`;
-        bookStatus.classList.add('bookStatus');
-        secondBookDiv.appendChild(bookStatus);
-
+        newCardFunction(i);
+        bookTitleFunction(i);
+        bookAuthorFunction(i);
+        secondBookDivFunction(i);
+        bookLengthFunction(i);
+        bookStatusFunction(i);
         cardContainer.appendChild(newCard);
 
     }
+    document.querySelectorAll('.bookStatus').forEach(addStatusListener)
 }
 
 function Book(title, author, pages, read) {
@@ -79,11 +131,15 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-
+    let newBook = new Book(
+        `${titleInput.value}`,
+        `${authorInput.value}`,
+        `${pagesInput.value}`,
+        `${readInput.checked}`,
+    )
+    myLibrary.push(newBook);
+    addCardsToPage();
 }
-
-
-
 
 addCardsToPage();
 
